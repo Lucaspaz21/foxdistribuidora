08b5925440a499508182210c5fd9c2ed89552f08
Lucaspaz21 <60491853+Lucaspaz21@users.noreply.github.com>
2023-06-07 22:19:09 -0300

    Update __init__.py

--- foxdistribuidora/__init__.py
+++ foxdistribuidora/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'c8adf5def159745c130e889bb6359899'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fox.db'
+
+if os.getenv("DATABASE_URL"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fox.db'
 
 database = SQLAlchemy(app)
 
